cdda97f318d78b0c63541f9cf1838f976c449c67
Akinsiku Femi <akinsiku13@gmail.com>
2020-06-23 09:26:49 +0100

    Add starter file for react-dark-mode

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App()
+{
+    return (
+        <div className="light-mode">
+            <nav>Toggle Goes Here</nav>
+            <main>
+                <h1>"Light Mode"</h1>
+                <h2>Toggle the switch to see some magic happen!</h2>
+            </main>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
